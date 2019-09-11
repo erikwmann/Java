0ee5af7fec3b54804959ab412118b8724960218a
Erik <emann@upei.ca>
2019-09-11 16:57:24 -0300

    Sudoku Start
    Making a Sudoku game. First starting off by making sure I understand the 2D array that will be used as the grid.

---
+++ Sudoku/Sudoku/Driver.java
@@ -0,0 +1,9 @@
+public class Driver{
+    public static void main(String[]args){
+    
+        System.out.println("Let's play Sudoku!");
+        
+        Grid grid = new Grid(0);
+        grid.printGrid();
+    }
+}
