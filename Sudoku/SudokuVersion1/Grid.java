bbf4f3d162d32a386745db264c9ff4c39f3e0fa1
Erik <emann@upei.ca>
2019-09-12 15:42:20 -0300

    Filling & Checks
    Created method to fill the grid with random numbers and checks for the rows and columns. Using a random number generator to fill the grid is not efficent. Need a better way to fill the grid. The checks do work.

--- Sudoku/SudokuVersion1/Grid.java
+++ Sudoku/SudokuVersion1/Grid.java
@@ -27,16 +27,64 @@ public class Grid
     }
     
     //Fills the grid with random numbers
-    private int[][] fillGrid(int size){
-        int num = random.nextInt(size)+1;//1-size
+    private void fillGrid(int size){
+        int num = random.nextInt(size)+1;//1 to size
         
         for(int i = 0; i<grid.length;i++){ 
-            for(int j = 0; j<grid.length;j++){
-                num = random.nextInt(size)+1;//1-size
+            for(int j = 0; j<grid.length;j++){   
+                num = random.nextInt(size)+1;//1 to size
                 grid[i][j] = num;
+                
+                //If the check come back false reset the squre and decrement j
+                if(!(checkRow(i) && checkCol(j) && checkSquare(i, j))){
+                    grid[i][j] = 0;
+                    j--;
+                }
             }
         }
+    }
+    
+    //Check a row for two of the same number
+    private boolean checkRow(int row){
+        int check [] = new int [grid.length+1];//Array 0 to n
         
-        return null;
+        //Goes through each row and adds 1 to the index of each number.
+        for(int i= 0; i<grid.length; i++){
+            check[grid[row][i]]+=1; 
+        }
+        
+        //check to see if more than one number is the same
+        //i starts at one because we don't care about 0s
+        for(int i= 1; i<check.length; i++){
+            if(check[i] > 1){
+                return false;
+            }
+        }
+        return true;
+    }
+    
+    //Check a column for two of the same number
+    private boolean checkCol(int col){
+        int check [] = new int [grid.length+1];//Array 0 to n
+        
+        //Goes through each column and adds 1 to the index of each number.
+        for(int i= 0; i<grid.length; i++){
+            check[grid[i][col]]+=1; 
+        }
+        
+        //check to see if more than one number is the same
+        //i starts at one because we don't care about 0s
+        for(int i= 1; i<check.length; i++){
+            if(check[i] > 1){
+                return false;
+            }
+        }
+        return true;
+    }
+    
+    //Check squares
+    private boolean checkSquare(int row, int column){
+        
+        return true;
     }
 }
