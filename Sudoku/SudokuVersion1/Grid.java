d1b04d632b75baa43562101b38dd4898e65d800a
Erik <emann@upei.ca>
2019-09-14 11:56:05 -0300

    Valid Grid
    Creates a valid Sudoku grid.

--- Sudoku/SudokuVersion1/Grid.java
+++ Sudoku/SudokuVersion1/Grid.java
@@ -1,5 +1,5 @@
 /*
- * A grid is an 2D array
+ * A Grid is an 2D array filled with numbers that are unique by row, column, and square.
  */
 
 import java.util.ArrayList;
@@ -11,7 +11,6 @@ public class Grid
     
     public Grid(int size){
         grid = new int [size][size];
-        
         fillGrid(size);
     }
     
@@ -39,7 +38,7 @@ public class Grid
             for(int j = 0; j<grid.length;j++){            
                 temp = nums.get(0);
                 grid[i][j] = temp; //get first element
-                nums.remove(0);
+                nums.remove(0); //remove the first element
                 //If the check come back false reset the squre and decrement j
                 if(!(checkRow(i) && checkCol(j) && checkSquare(i, j))){
                     grid[i][j] = 0; //reset the square
@@ -53,7 +52,6 @@ public class Grid
                     i--; //do the row over
                     j+=grid.length; //stop filling the row
                 }
-                //printGrid();
                 timeout++;
             }
             nums.clear();//removes all elements from the arraylist
@@ -70,9 +68,9 @@ public class Grid
     
     //Check a row for two of the same number
     private boolean checkRow(int row){
-        int check [] = new int [grid.length+1];//Array 0 to n
+        int check [] = new int [grid.length+1];//array 0 to n
         
-        //Goes through each row and adds 1 to the index of each number.
+        //goes through each row and adds 1 to the index of each number.
         for(int i= 0; i<grid.length; i++){
             check[grid[row][i]]+=1; 
         }
@@ -89,9 +87,9 @@ public class Grid
     
     //Check a column for two of the same number
     private boolean checkCol(int col){
-        int check [] = new int [grid.length+1];//Array 0 to n
+        int check [] = new int [grid.length+1];//array 0 to n
         
-        //Goes through each column and adds 1 to the index of each number.
+        //goes through each column and adds 1 to the index of each number.
         for(int i= 0; i<grid.length; i++){
             check[grid[i][col]]+=1; 
         }
@@ -106,9 +104,61 @@ public class Grid
         return true;
     }
     
-    //Check squares
-    private boolean checkSquare(int row, int column){
+    //Check squares in a 9x9 Grid
+    private boolean checkSquare(int row, int col){
+        int check [] = new int [grid.length+1];//array 0 to n
+        int rowEnd = 0;
+        int colEnd = 0;
         
+        //numbers are reduced beacuse the array starts at 0
+        //rows
+        //between 1 and 3
+        if(row <= 2){
+            row = 0;
+            rowEnd = 2;
+        }
+        //between 3 and 6
+        else if(row > 2 && row <= 5){
+            row = 3;
+            rowEnd = 5;
+        }
+        //between 7 and 9
+        else{
+            row = 6;
+            rowEnd = 8;
+        }
+        
+        //cols
+        //between 1 and 3
+        if(col <= 2){
+            col = 0;
+            colEnd = 2;
+        }
+        //between 3 and 6
+        else if(col > 2 && col <= 5){
+            col = 3;
+            colEnd = 5;
+        }
+        //between 7 and 9
+        else{
+            col = 6;
+            colEnd = 8;
+        }
+        
+        //go through the square and adds 1 to the index of each number.
+        for(int i = row; i <= rowEnd;i++){
+            for(int j = col; j <= colEnd;j++){
+                check[grid[i][j]]+=1; 
+            }
+        }
+        
+        //check to see if more than one number is the same
+        //i starts at one because we don't care about 0s
+        for(int i= 1; i<check.length; i++){
+            if(check[i] > 1){
+                return false;
+            }
+        }
         return true;
     }
 }
